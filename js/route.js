ef07b247d945b2df856472128e3b4e3a9bf27bd7
jerry7767 <death-J@hotmail.com>
2018-07-20 00:03:17 +0900

    fix path

---
+++ js/route.js
@@ -0,0 +1,17 @@
+angular.module('myApp', ['ngRoute'])
+.config(function($routeProvider, $locationProvider){
+
+
+    $routeProvider
+    .when("/main", {
+        templateUrl : "html/main.html"
+    })
+    .when("/menu", {
+        templateUrl : "html/menu.html"
+    })
+    .when("/about", {
+        templateUrl : "html/about.html"
+    })
+
+    //$locationProvider.html5Mode(true);
+});
