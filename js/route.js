37743553e42db5f112c1c2be35f89097eb236059
jerry7767 <death-J@hotmail.com>
2018-08-03 00:36:43 +0900

    add scroll animation

--- js/route.js
+++ js/route.js
@@ -61,3 +61,63 @@ app.controller('TranslateController', function($translate, $scope) {
         $translate.use(langKey);
     }
 });
+
+app.controller('HomePage', function($scope,$anchorScroll) {
+
+	function elmYPosition(eID) {
+		var elm = document.getElementById(eID);
+
+		var y = elm.offsetTop;
+		var node = elm;
+		while (node.offsetParent && node.offsetParent != document.body) {
+			node = node.offsetParent;
+			y += node.offsetTop;
+		} return y;
+	}
+
+	function currentYPosition() {
+		if (self.pageYOffset) {
+			return self.pageYOffset;
+		}
+
+		return 0;
+	}
+
+	function scrollTo(eID) {
+			var startY = currentYPosition();
+			var stopY = elmYPosition(eID);
+
+			var distance = stopY > startY ? stopY - startY : startY - stopY;
+			if (distance < 100) {
+				scrollTo(0, stopY);
+
+				return;
+			}
+
+			var speed = Math.round(distance / 100);
+			if (speed >= 20){
+				speed = 20;
+			}
+
+			var step = Math.round(distance / 100);
+			var leapY = stopY > startY ? startY + step : startY - step;
+			var timer = 0;
+			if (stopY > startY) {
+				for ( var i=startY; i<stopY; i+=step ) {
+					setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+					leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+				}
+
+				return;
+			}
+			for ( var i=startY; i>stopY; i-=step ) {
+				setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+				leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+			}
+		}
+
+
+	$scope.goToID = function (id) {
+		scrollTo("" + id);
+	}
+});
